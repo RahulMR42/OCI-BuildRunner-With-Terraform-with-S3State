dbbb1a25ba7fd2bc143337bedda8dedfec6077bc
fahdabidiroottenancy/Rahul <test@example.com>
2021-12-06 20:38:45 +0530

    v1.6

--- remote-backend.tf
+++ remote-backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
     bucket   = "terraform-backend"
-    key      = "networking/terraform.tfstate"
+    key      = "demo/terraform.tfstate"
     region   = "us-ashburn-1"
     endpoint = "https://fahdabidiroottenancy.compat.objectstorage.us-ashburn-1.oraclecloud.com"
-    shared_credentials_file     = "backend_credentials"
+    shared_credentials_file     = "cred_store"
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
