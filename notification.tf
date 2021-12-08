c3979e382fe0d4fcc7f6e8ecdb3e9e43c7b9c064
RahulMR <rahulmasquee@gmail.com>
2021-12-08 10:56:28 +0530

    added random

--- notification.tf
+++ notification.tf
@@ -2,7 +2,7 @@ resource "random_id" "tag" {
   byte_length = 2
 }
 
-resource "oci_ons_notification_topic" "test_notification_topic" {
+resource "oci_ons_notification_topic" "oci_ons_notification_topic" {
     #Required
     compartment_id = var.compartment_id
     name = "var.notification_topic_name-${random_id.tag.hex}"
@@ -11,4 +11,8 @@ resource "oci_ons_notification_topic" "test_notification_topic" {
     defined_tags = {"Operations.CostCenter"= "42"}
     description = var.notification_topic_description
     freeform_tags = {"Department"= "Finance"}
+}
+
+output "notifcation_details"{
+  vaule = oci_ons_notification_topic.oci_ons_notification_topic
 }
