b8cf0bea9703058e1808e237f1a55fc7599a6a18
RahulMR <rahulmasquee@gmail.com>
2021-12-08 12:10:54 +0530

    test

--- devops.tf
+++ devops.tf
@@ -6,5 +6,5 @@ resource "random_string" "deploy_id" {
 resource "oci_logging_log_group" "test_log_group" {
   compartment_id = var.compartment_ocid
   display_name   = "${var.app_name}_${random_string.deploy_id.result}_log_group"
-  defined_tags   = { "${oci_identity_tag_namespace.ArchitectureCenterTagNamespace.name}.${oci_identity_tag.ArchitectureCenterTag.name}" = var.release }
+  
 }
