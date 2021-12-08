bb68166bd69cc02cb25adcc42d9c1d14a6ded704
RahulMR <rahulmasquee@gmail.com>
2021-12-08 11:57:57 +0530

    new

--- remote-backend.tf
+++ remote-backend.tf
@@ -4,7 +4,7 @@ terraform {
     key      = "demo/terraform.tfstate"
     region   = "us-ashburn-1"
     endpoint = "https://fahdabidiroottenancy.compat.objectstorage.us-ashburn-1.oraclecloud.com"
-    shared_credentials_file     = "cred_store"
+    shared_credentials_file     = "./cred_store"
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
