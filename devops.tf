00fc906b83a28f8c92ff78989bbbb6f84445b279
RahulMR <rahulmasquee@gmail.com>
2021-12-09 15:16:24 +0530

    v2.0

--- devops.tf
+++ devops.tf
@@ -3,20 +3,12 @@ resource "random_string" "deploy_id" {
   special = false
 }
 
-# resource "oci_logging_log_group" "test_log_group" {
-#   compartment_id = var.compartment_ocid
-#   display_name   = "${var.app_name}_${random_string.deploy_id.result}_log_group"
-  
-# }
-
-# output "test_log_group"{
-#   value = oci_logging_log_group.test_log_group
-# }
-
-resource "oci_identity_dynamic_group" "devops_build_dg" {
-  provider       = oci.home_region
-  name           = "MyDynamicGroup"
-  description    = "DevOps repository build pipeline dynamic group"
+resource "oci_logging_log_group" "test_log_group" {
   compartment_id = var.compartment_ocid
-  matching_rule  = "ALL {resource.type = 'devopsbuildpipeline'}"
+  display_name   = "${var.app_name}_${random_string.deploy_id.result}_log_group"
+  
+}
+
+output "test_log_group"{
+  value = oci_logging_log_group.test_log_group
 }
